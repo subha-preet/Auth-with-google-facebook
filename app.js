8b4586329e4c5fb670dfe19b19607127a2cbae69
Subhapreet Kaur Rajwans <subhapreet@growthnatives.com>
2021-12-13 12:49:41 +0530

    starting the server

---
+++ app.js
@@ -0,0 +1,12 @@
+process.env.NODE_CONFIG_DIR = 'config/';
+var app_instance = process.argv.NODE_APP_INSTANCE;
+process.argv.NODE_APP_INSTANCE = "";
+config = require('config');
+process.argv.NODE_APP_INSTANCE = app_instance;
+
+var app = require('express')();
+global.app = app;
+
+require('./middlewares');
+require('./modules');
+require('./services/startupService').initializeServer();
